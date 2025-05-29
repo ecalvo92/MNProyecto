309ea7d0a024f95909f87be3c80847f0ea20be22
ecalvo92 <ecalvo@crnova.com>
2025-05-28 18:55:59 -0600

    Estructura Básica MVC

--- index.php
+++ index.php
@@ -1,32 +1,3 @@
-<html>
-
-    <head>
-        <link rel="stylesheet" href="estilos.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" >
-    </head>
-
-    <body>
-
-        <div class="row">
-            <div class="col-3">
-
-            </div>
-
-            <div class="col-6">
-            
-                 <!-- contenido -->
-                <h3>Identificación</h3>
-                <input type="text" id="txtIdentificacion" class="form-control"
-                onkeyup="ConsultarNombre2();">
-                <input type="text" id="txtNombre" class="form-control" readOnly="true">
-
-            </div>
-
-        </div>
-        
-    </body>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="funciones.js"></script>
-
-</html>
+<?php
+    header("location: Views/Home/login.php");
+?>
